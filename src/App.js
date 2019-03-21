52c1dab6e30065ec8658f36d76eaed924ca5468c
lautapercuspain <lautarogruss@gmail.com>
2019-03-21 15:35:32 -0700

    =Silvo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import silvo from './img/silvo.jpg';
 import './App.css';
+import './Animate.css';
+
+class Cat extends React.Component {
+	render() {
+		const { x, y } = this.props;
+		return (
+			<img
+				className="animated wobble infinite"
+				width="100px"
+				alt=""
+				src={silvo}
+				style={{ position: 'absolute', left: x, top: y }}
+			/>
+		);
+	}
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			x: 0,
+			y: 0
+		};
+	}
+	handleMouseMove = (event) => {
+		console.log('pasa x aqui::');
+		this.setState({
+			x: event.clientX,
+			y: event.clientY
+		});
+	};
+
+	render() {
+		return (
+			<div className="App" onMouseMove={this.handleMouseMove}>
+				<header className="App-header">
+					<h1 className="animated slideInDown flipInX  infinite ">Yo soy el Silvo!!</h1>
+					<Cat x={this.state.x} y={this.state.y} />
+					{/* <img src={logo} className="App-logo" alt="logo" /> */}
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
