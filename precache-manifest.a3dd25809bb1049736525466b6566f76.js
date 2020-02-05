1a31b56edeee928ff751fe6c0d259cddcb08fb7d
lautapercuspain <lautarogruss@gmail.com>
2020-02-05 13:15:04 -0700

    Updates

---
+++ precache-manifest.a3dd25809bb1049736525466b6566f76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ef5b81cd60fa20e3403",
+    "url": "/silvo-app/static/css/main.1cabdf3a.chunk.css"
+  },
+  {
+    "revision": "4ef5b81cd60fa20e3403",
+    "url": "/silvo-app/static/js/main.db38a936.chunk.js"
+  },
+  {
+    "revision": "2cac4868ea750fbff038",
+    "url": "/silvo-app/static/js/runtime~main.125216b6.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "/silvo-app/static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "c9a09ccffb04078cb58978a8805f78ab",
+    "url": "/silvo-app/static/media/silvo.c9a09ccf.jpg"
+  },
+  {
+    "revision": "11ca54abc140a31bdc4ccb4e69356d10",
+    "url": "/silvo-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.1858c818c87c2160449eac312146eee1.js"
+  "/silvo-app/precache-manifest.a3dd25809bb1049736525466b6566f76.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/silvo-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
