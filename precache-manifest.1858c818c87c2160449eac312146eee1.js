bd0c27c513e1ef92075a6d45ecdac41326a76746
lautapercuspain <lautarogruss@gmail.com>
2020-02-05 13:12:49 -0700

    Updates

---
+++ precache-manifest.1858c818c87c2160449eac312146eee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22b5ce6f268d11651669",
+    "url": "/my-app/static/css/main.1cabdf3a.chunk.css"
+  },
+  {
+    "revision": "22b5ce6f268d11651669",
+    "url": "/my-app/static/js/main.03f9969a.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "/my-app/static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "c9a09ccffb04078cb58978a8805f78ab",
+    "url": "/my-app/static/media/silvo.c9a09ccf.jpg"
+  },
+  {
+    "revision": "8db3a5da4a33cf6181ecde921349fb55",
+    "url": "/my-app/index.html"
+  }
+];
